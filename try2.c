c1965f31dc37d18b046c902dbbc80b2a016b50c5
Just-a-guy-o7 <pandey1divyanshu@gmail.com>
2024-11-24 00:37:01 +0530

    added arrays for walls

--- try2.c
+++ try2.c
@@ -1,39 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void printHori(int size){
-    if(size==0){
-        return;
-    }
-    printHori(size-1);
-    printf("._");
-}
-void printVer(int size){
-    if(size==0){
-        return;
-    }
-    printVer(size-1);
-    printf("| ");
-}
+
 
 int main(int argc, char const *argv[])
-{   
-//  int upper_bound=5;
-//  int lower_bound=1;
-
-//  int value = (rand() % (upper_bound - lower_bound + 1)
-//                     + lower_bound);
- 
+{ 
     int size;
     scanf("%d",&size);
-    for(int i=0;i<size;i++){
-        printHori(size-1);
-        printf(".\n");
-        printVer(size-1);
-        printf("|\n");
-    }
-    printHori(size-1);
-    printf(".\n");
+    int horiwalls[size*size+1][2];
+    int verticalwalls[size*size+1][2];
+    int path[size*size][2];
+    int horicount=0,vertcount=0;
 
+    for(int i=0;i<size;i++){
+        horiwalls[horicount][0]=0;
+        horiwalls[horicount++][1]=i;
+        horiwalls[horicount][0]=size;
+        horiwalls[horicount++][1]=i;
+        verticalwalls[vertcount][0]=i; 
+        verticalwalls[vertcount++][1]=0; 
+        verticalwalls[vertcount][0]=i; 
+        verticalwalls[vertcount++][1]=size; 
+    }
     
+
+
 }
